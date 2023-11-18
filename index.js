f25d0943f6c08eec83aa4ab709fd0c0650a955e3
Lordkaito <cespedesisai88@gmail.com>
2023-11-18 01:28:56 +0100

    Update jest config to use babel

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import puppeteer from "puppeteer";
 
 const URL = "https://news.ycombinator.com/";
 
-const getWebpage = async () => {
-  console.log("getWebpage");
-  return "test"
+const getWebpage = async (url) => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  return url
 };
 
 export default getWebpage;
--- tests/getWebPage.test.js
+++ tests/getWebPage.test.js
@@ -1,11 +1,10 @@
-const puppeteer = require("puppeteer");
 import getWebpage from "../index";
 
 const URL = "https://news.ycombinator.com/";
 
 test("gets web page", async () => {
 
-  const webpageResult = await getWebpage();
-  expect(webpageResult).toBe("test");
+  const getWebpageResult = await getWebpage(URL);
+  expect(getWebpageResult).toBe(URL);
   return;
 });
