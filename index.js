3b684afb1c069f0c2f815ff7e3d845742996afe6
Lordkaito <cespedesisai88@gmail.com>
2023-11-18 02:18:52 +0100

    Create basic scrappet, organize code in small functions

--- index.js
+++ index.js
@@ -2,14 +2,65 @@ import puppeteer from "puppeteer";
 
 const URL = "https://news.ycombinator.com/";
 
-const getWebpage = async (url) => {
-  const browser = await puppeteer.launch();
-
+async function index() {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    defaultViewport: null,
+  });
   const page = await browser.newPage();
+  await page.goto(URL, {
+    waitUntil: "domcontentloaded",
+  });
+  const rows = await getRows(page);
+  const points = await getPoints(page);
+  const comments = await getComments(page);
 
-  await page.goto(url);
+  console.log(
+    rows,
+    points,
+    comments,
+    rows.map((row) => {
+      return row.rank;
+    })
+  );
+  return rows;
+}
 
-  return url
-};
+async function getRows(page) {
+  return await page.evaluate(() => {
+    const rows = document.querySelectorAll("tr.athing");
 
-export default getWebpage;
+    return Array.from(rows).map((row) => {
+      const rank = row.querySelector("td span.rank").innerText;
+      const title = row.querySelector("td.title span.titleline").innerText;
+      const id = row.getAttribute("id");
+      return {
+        rank,
+        title,
+        id,
+      };
+    });
+  });
+}
+
+async function getPoints(page) {
+  return await page.evaluate(() => {
+    const points = document.querySelectorAll("td.subtext span.score");
+
+    return Array.from(points).map((point) => {
+      return point.innerText;
+    });
+  });
+}
+
+async function getComments(page) {
+  return await page.evaluate(() => {
+    const comments = document.querySelectorAll("td.subtext a:last-child");
+
+    return Array.from(comments).map((comment) => {
+      return comment.innerText;
+    });
+  });
+}
+
+index();
--- tests/getWebPage.test.js
+++ tests/getWebPage.test.js
@@ -3,8 +3,15 @@ import getWebpage from "../index";
 const URL = "https://news.ycombinator.com/";
 
 test("gets web page", async () => {
-
   const getWebpageResult = await getWebpage(URL);
+
   expect(getWebpageResult).toBe(URL);
+  return;
+});
+
+test("gets the 30 rows", async () => {
+  const getWebpageResult = await getWebpage(URL);
+
+  expect(getWebpageResult).toHaveLength(30);
   return;
 });
